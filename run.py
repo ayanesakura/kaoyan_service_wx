6e6d3a9a90ea998a3cbd45460cfa0e689337223a
Ayane <wangruofei@soulapp.cn>
2025-02-01 22:12:44 +0800

    fix bug

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # 创建应用实例
 import sys
+import threading
 from wxcloudrun import app, download_file
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    # 先下载文件
-    download_file()
+    # 在后台线程中下载文件
+    threading.Thread(target=download_file, daemon=True).start()
     # 启动服务
     app.run(host=sys.argv[1], port=sys.argv[2])
--- wxcloudrun/apis/choose_schools.py
+++ wxcloudrun/apis/choose_schools.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 from wxcloudrun.utils.file_util import loads_json
 from flask import request, jsonify
 from typing import List, Dict
+import time
 
 # 加载学校基础数据
 cur_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,21 +13,40 @@ cur_dir = os.path.dirname(cur_dir)
 
 SCHOOL_DATA_PATH = os.path.join(cur_dir, 'resources/rich_fx_flat_v2.json')
 
-SCHOOL_DATAS = loads_json(SCHOOL_DATA_PATH)
-
+# 全局变量存储数据
+SCHOOL_DATAS = None
 city_level_map = {
     '211': set(),
     '985': set(),
     'c9': set(['北京大学', '清华大学', '复旦大学', '上海交通大学', '浙江大学', '南京大学', '中国科学技术大学', '哈尔滨工业大学', '西安交通大学'])
 }
 
-for data in SCHOOL_DATAS:
-    school_name, is_985, is_211 = data['school_name'], data['is_985'], data['is_211']
-    if is_985:
-        city_level_map['985'].add(school_name)
-    if is_211:
-        city_level_map['211'].add(school_name)
-
+def load_school_data():
+    """加载学校数据，如果文件不存在则等待并重试"""
+    global SCHOOL_DATAS, city_level_map
+    
+    max_retries = 30  # 最多等待30秒
+    retry_count = 0
+    
+    while retry_count < max_retries:
+        try:
+            if os.path.exists(SCHOOL_DATA_PATH):
+                SCHOOL_DATAS = loads_json(SCHOOL_DATA_PATH)
+                # 更新city_level_map
+                for data in SCHOOL_DATAS:
+                    school_name, is_985, is_211 = data['school_name'], data['is_985'], data['is_211']
+                    if is_985:
+                        city_level_map['985'].add(school_name)
+                    if is_211:
+                        city_level_map['211'].add(school_name)
+                return True
+        except Exception as e:
+            print(f"Error loading school data: {e}")
+        
+        retry_count += 1
+        time.sleep(1)  # 等待1秒后重试
+    
+    return False
 
 def is_target_match(target_info, school_info):
     # 获取目标信息中的各个字段,不存在则为空
@@ -67,11 +87,16 @@ def is_target_match(target_info, school_info):
     
     return target_school_flag and major_flag and direction_flag and city_or_province_flag and school_level_flag
 
-
 def choose_schools():
+    # 确保数据已加载
+    if SCHOOL_DATAS is None and not load_school_data():
+        return jsonify({
+            'code': 500,
+            'message': '学校数据加载失败，请稍后重试'
+        })
+
     request_data = request.get_json()
     user_info, target_info = request_data.get('user_info', {}), request_data.get('target_info', {})
-    # print(request_data)
     sort_info = request_data.get('sort_info', [])
     # 检查target_info中是否至少有一个字段有值
     school, major, city, school_level = target_info.get('school'), target_info.get('major'), target_info.get('city'), target_info.get('school_level')
@@ -82,10 +107,6 @@ def choose_schools():
         })
     
     target_schools = [s for s in SCHOOL_DATAS if is_target_match(target_info, s)]
-
-    # # 根据sort_info进行排序
-    # if sort_info:
-    #     target_schools = sorted(target_schools, key=lambda x: [info['sort_key'] for info in sort_info if info['sort_key'] in x])
 
     return jsonify({
         'code': 200,
